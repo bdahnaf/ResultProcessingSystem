29cfcb89140ff7c12f57433fb54892fc25b1edcc
Ahnaf Shahriar <bdahnaf@gmail.com>
2024-04-09 23:27:06 +0600

    Add Session model

---
+++ Models/Session.cs
@@ -0,0 +1,9 @@
+﻿namespace ResultProcessingSystem.Models
+{
+    public class Session
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string NameBn { get; set; }
+    }
+}
