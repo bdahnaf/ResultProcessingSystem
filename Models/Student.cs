e8dc9fa6b08ba039c0a6e5ad29c598304e26c8ba
Ahnaf Shahriar <bdahnaf@gmail.com>
2024-04-09 23:20:52 +0600

    Add Student Model

---
+++ Models/Student.cs
@@ -0,0 +1,16 @@
+﻿namespace ResultProcessingSystem.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string NameBn { get; set; }
+        public string FathersName { get; set; }
+        public string MothersName { get; set; }
+        public int SessionId { get; set; }
+        public int ClassId { get; set; }
+        public int RollNumber { get; set; }
+        public int Status { get; set; }
+        public int IsActive { get; set; }
+    }
+}
