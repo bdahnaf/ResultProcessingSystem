fb5a2938be98c528cfc7b9d3d1fab4a62dea3b4b
Ahnaf Shahriar <bdahnaf@gmail.com>
2024-04-09 23:14:45 +0600

    Add Role model

---
+++ Models/Role.cs
@@ -0,0 +1,12 @@
+﻿namespace ResultProcessingSystem.Models
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string NameBn { get; set; }
+        public string Description { get; set; }
+        public string DescriptionBn { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
