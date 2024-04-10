06ac134ab1a01f1a936f319243f01223f026262a
Ahnaf Shahriar <bdahnaf@gmail.com>
2024-04-11 00:14:59 +0600

    WIP: Exam and Mark relation

---
+++ Models/ExamExamSubjectExamSubjectMark.cs
@@ -0,0 +1,11 @@
+﻿namespace ResultProcessingSystem.Models
+{
+    public class ExamExamSubjectExamSubjectMark
+    {
+        public int ExamExamSubjectId { get; set; }
+        public ExamExamSubject ExamExamSubject { get; set; }
+
+        public int ExamSubjectMark { get; set; }
+        public ExamSubjectMark ExamSubjectMark { get; set; }
+    }
+}
---
+++ Models/ExamSubjectMark.cs
@@ -0,0 +1,9 @@
+﻿namespace ResultProcessingSystem.Models
+{
+    public class ExamSubjectMark
+    {
+        public int Id { get; set; }
+        public decimal FullMark { get; set; }
+        public decimal PassMark { get; set; }
+    }
+}
