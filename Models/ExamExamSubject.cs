53332b6328179d60e6482e9fe7930d81620721c1
Ahnaf Shahriar <bdahnaf@gmail.com>
2024-04-10 23:53:31 +0600

    m2m Exam and ExamSubject

---
+++ Models/ExamExamSubject.cs
@@ -0,0 +1,11 @@
+﻿namespace ResultProcessingSystem.Models
+{
+    public class ExamExamSubject
+    {
+        public int ExamId { get; set; }
+        public Exam Exam { get; set; }
+
+        public int ExamSubjectId { get; set; }
+        public ExamSubject ExamSubject { get; set; }
+    }
+}
