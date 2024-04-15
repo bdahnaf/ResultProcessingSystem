4467a3e8a1e400ce5a788c1d51fd907f8d926911
Ahnaf-DDC-LPTP <bdahnaf@gmail.com>
2024-04-15 15:24:52 +0600

    Add Result Model

---
+++ Models/Result.cs
@@ -0,0 +1,10 @@
+﻿namespace ResultProcessingSystem.Models
+{
+    public class Result
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public int ExamExamSubjectId { get; set; }
+        public decimal Marks { get; set; }
+    }
+}
